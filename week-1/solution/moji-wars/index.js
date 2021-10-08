344e4c2488d6de014fee4e5b5058e1e499cdb4e4
emsesc <emseschen@gmail.com>
2021-10-08 19:29:19 -0400

    working solution

--- week-1/solution/moji-wars/index.js
+++ week-1/solution/moji-wars/index.js
@@ -1,12 +1,15 @@
 var translator = require("emoji-translator")
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    let redacted = req.query.redacted
+    context.log('HTTP Function has received redacted text: ' + redacted);
 
-
+    const vocabulary = { '🕵🏽': 'John', '🕵🏻‍♀️': 'Nora', '🗻': 'Waverly Valley Place' };
+    const translate = translator(vocabulary)
+    const string = translate(redacted)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: string
     };
 }
